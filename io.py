937b383aa08ba33029f890a53c02c93e9176b820
semyeong@eunomia <semyeong.oh@gmail.com>
2014-05-07 21:19:19 -0400

    downloadfile added

--- io.py
+++ io.py
@@ -1,10 +1,15 @@
 """ Functions related to I/O """
 
+import urllib
 import os, errno
+from urllib2 import urlopen, URLError, HTTPError
 
-__all__ = ['mkdir_p']
+__all__ = ['mkdir_p', 'downloadfile']
 
 def mkdir_p(path):
+    """
+    Mimic mkdir -p
+    """
     try:
         os.makedirs(path)
     except OSError as exc: # Python >2.5
@@ -12,3 +17,35 @@ def mkdir_p(path):
             pass
         else: raise
 
+def downloadfile(url, savename=None, prefix=None):
+    """
+    Download a file from url
+
+    url : url
+    savename : target filename (default=basename of url)
+    prefix : directory prefix (created if not there)
+    """
+    if not savename:
+        savename = os.path.basename(url)
+    if not prefix:
+        prefix = './'
+
+    mkdir_p(prefix)
+    try:
+        f = urlopen(url)
+        localname = prefix + '/' + savename
+        if os.path.isfile(localname):
+            pass
+        else:
+            # Open our local file for writing
+            with open(localname, "wb") as local_file:
+                local_file.write(f.read())
+                local_file.close()
+        return 1
+    #handle errors
+    except HTTPError, e:
+        print "HTTP Error:", e.code, url
+        return 0
+    except URLError, e:
+        print "URL Error:", e.reason, url
+        return 0
