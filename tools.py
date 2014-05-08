059378dc9b772ba63fa22106c832712b2142cf82
semyeong@eunomia <semyeong.oh@gmail.com>
2014-05-07 21:25:27 -0400

    linebreak

--- tools.py
+++ tools.py
@@ -2,36 +2,27 @@
 
 import pylab as pl
 
-__all__ = ['set_pub_single', 'get_handle', 'myfigure', 'hide_tick_labels', 'ImageWithColorbar']
+__all__ = ['set_pub_single', 'set_ipynb', 'get_handle', 'myfigure',
+            'hide_tick_labels', 'ImageWithColorbar', 'divide_axes',
+            'plotdiff']
 
 def set_pub_single():
     """set rc parameters for publication (single plot)"""
 
-    ### FONTS
-    font_dict = {'family':'Times New Roman'}  # Serif font
-    # font_dict = {'family':'Trebuchet MS'}  # Sans-Serif font
-    pl.rc('font', **font_dict)
-
-
     ### TICKS
     tick_maj_size = 10
     tick_maj_pad  = 5
-
     tick_min_size = 5
     tick_min_pad  = 5
-
-    tick_labelsize = 20
-
-
+    tick_labelsize = 15
     tick_dict = {'major.size':tick_maj_size, 'major.pad':tick_maj_pad,
                  'minor.size':tick_min_size, 'minor.pad':tick_min_pad,
                  'labelsize':tick_labelsize}
-
     pl.rc('xtick', **tick_dict)
     pl.rc('ytick', **tick_dict)
 
     linewidth = 1
-    axes_labelsize = 20
+    axes_labelsize = 15
 
     ### AXES
     pl.rc('axes', lw=linewidth, labelsize=axes_labelsize)
@@ -39,10 +30,39 @@ def set_pub_single():
     ### LINES
     pl.rc('lines', lw=linewidth, color='k', mew=linewidth) 
 
+    pl.rc('legend', numpoints=1, scatterpoints=1, frameon=False)
+
     ### FIGURE
-    # pl.rc('figure', facecolor='w', figsize=(4, 3))
+    pl.rc('figure.subplot', left=0.15, bottom=0.15, top=0.95, right=0.95)
 
     pl.rc('savefig', dpi=150)       # higher res outputs
+
+    pl.rc('mathtext', default='regular')
+
+
+def set_ipynb():
+    ### TICKS
+    tick_maj_size = 10
+    tick_maj_pad  = 5
+    tick_min_size = 5
+    tick_min_pad  = 5
+    tick_labelsize = 12
+    tick_dict = {'major.size':tick_maj_size, 'major.pad':tick_maj_pad,
+                 'minor.size':tick_min_size, 'minor.pad':tick_min_pad,
+                 'labelsize':tick_labelsize}
+    pl.rc('xtick', **tick_dict)
+    pl.rc('ytick', **tick_dict)
+    linewidth = 1
+    axes_labelsize = 14
+    ### AXES
+    pl.rc('axes', lw=linewidth, labelsize=axes_labelsize)
+    ### LINES
+    pl.rc('lines', lw=linewidth, color='k', mew=linewidth) 
+    pl.rc('legend', numpoints=1, scatterpoints=1, frameon=False)
+    pl.rc('patch', edgecolor='None')
+    ### FIGURE
+    pl.rc('figure', figsize=(6,4))
+    pl.rc('figure.subplot', left=0.15, bottom=0.15, top=0.95, right=0.95)
 
     pl.rc('mathtext', default='regular')
 
@@ -113,3 +133,28 @@ def ImageWithColorbar(ax, image, **kwargs):
     ax_cb.xaxis.tick_bottom()
 
     return ax, cb
+
+
+def divide_axes(ax, loc="bottom", size="30%", pad=0.05):
+    """
+    divide axes
+    """
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    divider = make_axes_locatable(ax)
+    ax_sub = divider.append_axes(loc, size=size, pad=pad)
+    return ax, ax_sub
+
+
+def plotdiff(ax, x, y1, y2, ratio=False, **kwargs):
+    """
+    Plot difference y2-y1 or ratio y2/y1 in a subpanel
+    """
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    divider = make_axes_locatable(ax)
+    ax_diff = divider.append_axes("bottom", size="30%", pad=0.05)
+    ax.get_figure().add_axes(ax_diff)
+    if ratio:
+        ax_diff.plot(x, y2/y1, **kwargs)
+    else:
+        ax_diff.plot(x, y2-y1, **kwargs)
+    return ax, ax_diff
